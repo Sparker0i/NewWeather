7f878ed622ca8575ed22e81b5391318a907deade
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-08 06:14:12 +0530

    Create Fake Raw Data

--- app/src/main/java/com/a5corp/myapplication/MainActivityFragment.java
+++ app/src/main/java/com/a5corp/myapplication/MainActivityFragment.java
@@ -6,6 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +22,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] forecastArray = {
+                "Today - Sunny 88/63",
+                "Tomorrow - Foggy 70/40",
+                "Today - Sunny 88/63",
+                "Tomorrow - Foggy 70/40",
+                "Today - Sunny 88/63",
+                "Tomorrow - Foggy 70/40",
+                "Today - Sunny 88/63",
+                "Tomorrow - Foggy 70/40"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(
+                Arrays.asList(forecastArray)
+        );
+        
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
