c573bcaf34784fbde49ffd70afa10780db76eb40
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-11 03:24:15 +0530

    Working Settings Activity

---
+++ app/src/main/java/com/a5corp/weather/SettingsActivityFragment.java
@@ -0,0 +1,29 @@
+package com.a5corp.weather;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    public SettingsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object o) {
+        return false;
+    }
+}
