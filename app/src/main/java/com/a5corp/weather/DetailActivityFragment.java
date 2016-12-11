a61df47aad3250c36cf178fc30af85c02c8fa2ae
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-12 05:29:40 +0530

    Working Share Intent

--- app/src/main/java/com/a5corp/weather/DetailActivityFragment.java
+++ app/src/main/java/com/a5corp/weather/DetailActivityFragment.java
@@ -1,9 +1,13 @@
 package com.a5corp.weather;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,7 +17,27 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private ShareActionProvider mShareActionProvider;
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_TAG = "\n\nSent from Simple Weather by @Sparker0i";
+    private String mForecastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+        ShareActionProvider provider = (ShareActionProvider) MenuItemCompat.getActionProvider(menu.findItem(R.id.action_share));
+
+        if (provider != null) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_TAG);
+            shareIntent.setType("text/plain");
+            provider.setShareIntent(shareIntent);
+        }
     }
 
     @Override
@@ -22,9 +46,18 @@ public class DetailActivityFragment extends Fragment {
         Intent intent = getActivity().getIntent();
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
         }
         return rootView;
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_TAG);
+        return shareIntent;
     }
 }
