5f5e6507a8252dd1389a013846bf66c1dfa4f4bc
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-11 23:09:09 +0530

    Revert "Working Settings Modifier"
    This reverts commit 64aeb8073722121b9530687d88e5b8c461e6e5ad.

--- app/src/main/java/com/a5corp/weather/SettingsActivity.java
+++ app/src/main/java/com/a5corp/weather/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.a5corp.weather;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,9 +15,16 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getFragmentManager().beginTransaction()
-                .replace(R.id.container, new SettingsActivityFragment())
-                .commit();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
--- app/src/main/java/com/a5corp/weather/SettingsActivityFragment.java
+++ app/src/main/java/com/a5corp/weather/SettingsActivityFragment.java
@@ -1,10 +1,8 @@
 package com.a5corp.weather;
 
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,33 +19,11 @@ public class SettingsActivityFragment extends PreferenceFragment implements Pref
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        addPreferencesFromResource(R.xml.pref_general);
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_default)));
         return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
-
-    public void bindPreferenceSummaryToValue(Preference preference) {
-        preference.setOnPreferenceChangeListener(this);
-
-        onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object o) {
-        String value = (String) o;
-        if (preference instanceof ListPreference) {
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(value);
-            if (prefIndex >= 0)
-                preference.setSummary(listPreference.getEntries()[prefIndex]);
-        }
-        else {
-            preference.setSummary(value);
-        }
-        return true;
+        return false;
     }
 }
